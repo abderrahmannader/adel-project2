00a6ff8641f36c801b59da92f422e26a0d6d1ecb
abdo <abderrahman.nader10@gmail.com>
2024-01-07 19:49:12 +0200

    done

--- js folder/java.js
+++ js folder/java.js
@@ -407,10 +407,16 @@ function offerPage(){
          
         </ul>
       </div>
+
+      <div onclick="arabicChange()" class="arabic-icon ">
+                <img class="w-100" src="img/saudi-arabia.png" alt="">
+              </div>
     </div>
   </nav>
 
-  <section class="container">
+  
+
+  <section  id="main-container-submit" class="container">
     <div class="form-control bg-black w-75 m-auto p-5 border border-5">
 
         <h2 class="text-center  pb-5 fw-bolder plan-name-holder offers-section-words">${PlanName}</h2>
@@ -732,4 +738,182 @@ async function GetData() {
 
 
 
+function arabicChange(){
+
+    let main = document.querySelector("#main-container-submit")
+    let PlanName = document.querySelector(".offers-section-words").innerHTML
+
+    
+    console.log(PlanName)
+
+    if(PlanName == "Economy Plan"){
+        PlanName = "الخطة الاقتصادية"
+    }
+    if (PlanName == "Premuim Plan") {
+        PlanName = "الخطةالمميزة"
+        
+    }
+    if (PlanName == "VIP Plan") {
+        PlanName = "الخطةالخاصة"
+        
+    }
+        
+        
+    
+
+
+    main.setAttribute("dir", "rtl")
+
+    main.innerHTML = `
+    <div class="form-control bg-black w-75 m-auto p-5 border border-5">
+    
+        <h2 class="text-center  pb-5 fw-bolder application offers-section-words">${PlanName}</h2>
+        <label for="Name" class="text-white pb-3 fs-5 name">الاسم :</label>
+        <input id="Name" class="form-control mb-4" type="text" placeholder="اسمك" aria-label="default input example">
+        
+        <label for="Weight" class="text-white pb-3 fs-5 weight">الوزن :</label>
+        <input id="Weight" class="form-control mb-4" type="text" placeholder="اكتب وزنك" aria-label="default input example">
+        
+        <label for="phone" class="text-white pb-3 fs-5 Phone">رقم الوتساب :</label>
+        <input id="phone" class="form-control mb-4" type="text" placeholder="اكتب رقم الوتساب الخاص بك" aria-label="default input example">
+    
+        <label for="Age" class="text-white pb-3 fs-5 age">السن :</label>
+        <input id="Age" class="form-control mb-4" type="text" placeholder="اكتب سنك" aria-label="default input example">
+    
+        <label for="injuries" class="text-white pb-3 fs-5 ">هل يوجد اي اصابات ؟ :</label>
+        <select id="injuries" onclick="injuriesArabic()"  class="form-select mb-4 FalseOption" aria-label="Default select example">
+            <option class="open-window" selected value="2">افتح النافذة</option>
+            <option class="yes"   value="1">نعم</option>
+            <option class="no"  value="2">لا</option>
+          </select>
+          <div class="injuries">
+          </div>
+    
+    
+          <label for="" class="text-white pb-3 fs-5  medical-conditions">هل يوجد اي امراض او مشاكل صحيه ؟ :</label>
+          <select id="medical" onchange=" medicalArabic()"  class="form-select mb-4 FalseOption2" aria-label="Default select example">
+            <option class="open-window" selected value="2">افتح النافذة</option>
+              <option class="yes"   value="1">نعم</option>
+              <option class="no"  value="2">لا</option>
+            </select>
+            <div class="medical">
+            </div>
+    
+    
+            <label for="" class="text-white pb-3 fs-5 allergies">هل يوجد حساسيه تجاه انواع معينه من الطعام ؟  :</label>
+          <select id="allergies" onclick="foodArabic()"  class="form-select mb-4 FalseOption3" aria-label="Default select example">
+            <option class="open-window" selected value="2">افتح النافذة</option>
+              <option class="yes"   value="1">نعم</option>
+              <option class="no"  value="2">لا</option>
+            </select>
+            <div class="food">
+            </div>
+    
+            <label for="" class="text-white pb-3 fs-5 Favorite-food">الطعام المفضل :</label>
+            <textarea id="favorite"  class="form-control mb-4"  placeholder="اشرح" rows="3"></textarea>
+    
+            <label for="" class="text-white pb-3 fs-5 Least-favorite">الطعام الغير مفضل :</label>
+            <textarea id="Least"  class="form-control mb-4"  placeholder="اشرح" rows="3"></textarea>
+    
+            <label for="" class="text-white pb-3 fs-5 gym-equipment">هل يوجد خبره بالتمارين والاجهزه الرياضيه فالجيم ؟ :</label>
+          <select id="xp"   class="form-select mb-4 " aria-label="Default select example">
+            <option class="open-window" selected value="2">افتح النافذة</option>
+              <option class="yes"   value="1">نعم</option>
+              <option class="no"  value="2">لا</option>
+            </select>
+    
+    
+    
+    
+            <label for="" class="text-white pb-3 fs-5 daily-effort">قيم مجهودك اليومي  :</label>
+          <select id="daily-effort"   class="form-select mb-4 " aria-label="Default select example">
+            <option class="open-window" selected value="">افتح النافذة</option>
+              <option class="little-effort"  value=" Exert little effort">ابذل مجهود قليل</option>
+              <option class="moderate-effort"  value="Exert moderate effort">ابذل مجهود متوسط</option>
+              <option  class="high-effort" value="Exert high effort">ابذل مجهود عالي  </option>
+            </select>
+    
+            <label for="" class="text-white pb-3 fs-5 working-hours ">حدد مواعيد عملك :</label>
+            <textarea id="work-hours"  class="form-control mb-4"  placeholder="اشرح" rows="3"></textarea>
+    
+            <label for="" class="text-white pb-3 fs-5 sleeping-schedule">حدد مواعيد النوم :</label>
+            <textarea id="sleep"  class="form-control mb-4"  placeholder="اشرح" rows="3"></textarea>
+    
+            <label for="" class="text-white pb-3 fs-5 achieve">هل تريد ان تصل لهدفك :</label>
+          <select id="achieve-goal"   class="form-select mb-4 " aria-label="Default select example">
+            <option selected value="">افتح النافذة</option>
+              <option   value="in a short time with a strong plan">في وقت قصير لكن النظام سيكون قوي         </option>
+              <option  value="moderate time with a moderate and balanced plan">في وقت متوسط والنظام سيكون بشده متوسطه ومعتدله</option>
+              
+            </select>
+    
+    
+    
+          
+    
+            <label for="duration" class="text-white pb-3 fs-5">اجمالي مده الخبره :</label>
+            <input id="duration" class="form-control mb-4" type="text" placeholder="سنوات الخبرة" aria-label="default input example">
+    
+            <label for="goal" class="text-white pb-3 fs-5 ">حدد واوصف هدفك بشكل كامل :</label>
+            <textarea id="goal"  class="form-control mb-4"  placeholder="اشرح" rows="3"></textarea>
+    
+            <label for="adel" class="text-primary pb-3 fs-5 ">اكتب كل ماتريد ل دكتور عادل :</label>
+            <textarea id="adel"  class="form-control mb-4"  placeholder="اشرح" rows="3"></textarea>
+    
+    
+            <button onclick="GetData()"  class="btn btn-primary button-main mb-5 py-2 px-4 mt-4 col-lg-3  col-md-6 ">ارسال</button>
+    
+    
+            
+    
+    
+    
+      </div>`
+}
+
+
+function injuriesArabic(){
+    let injuriesFunction = document.querySelector(".injuries")
+    let FalseOption = document.querySelector(".FalseOption")
+
+    if(FalseOption.value == 2){
+        injuriesFunction.innerHTML = ``
+
+    }else{
+        injuriesFunction.innerHTML = ` <label for="explan" class=" pb-3 fs-5 text-warning">اشرح :</label>
+        <textarea  class="form-control mb-4" id="injuriesExplan" placeholder="اشرح هنا" rows="3"></textarea>`
+    }
+    
+    
+}
+
+function medicalArabic(){
+    let injuriesFunction = document.querySelector(".medical")
+    let FalseOption = document.querySelector(".FalseOption2")
+
+    if(FalseOption.value == 2){
+        injuriesFunction.innerHTML = ``
+
+    }else{
+        injuriesFunction.innerHTML = ` <label for="explan" class=" pb-3 fs-5 text-warning">اشرح :</label>
+        <textarea  class="form-control mb-4" id="medicalexplan" placeholder="اشرح هنا" rows="3"></textarea>`
+    }
+    
+    
+}
+
+function foodArabic(){
+    let injuriesFunction = document.querySelector(".food")
+    let FalseOption = document.querySelector(".FalseOption3")
+
+    if(FalseOption.value == 2){
+        injuriesFunction.innerHTML = ``
+
+    }else{
+        injuriesFunction.innerHTML = ` <label for="explan " class=" pb-3 fs-5 text-warning">اشرح :</label>
+        <textarea  class="form-control mb-4" id="foodexplan" placeholder="اشرح هنا" rows="3"></textarea>`
+    }
+    
+    
+}
 
